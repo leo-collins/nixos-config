e5e6f24b6e6840c8344670027c5cc683df8e1d9e
Leo Collins <collins.leo@proton.me>
2026-06-25 19:05:27 +0100

    add network manager config

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,27 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  networking.networkmanager.ensureProfiles.profiles = {
+    "living-room-nixos" = {
+      connection = {
+        id = "living-room-nixos";
+        type = "ethernet";
+        interface-name = "enp0s31f6";
+        autoconnect = true;
+      };
+
+      ipv4 = {
+        method = "manual";
+        address1 = "192.168.0.234/24,192.168.0.1";
+        dns = "9.9.9.9;1.1.1.1";
+      };
+
+      ipv6 = {
+        method = "auto";
+      };
+    };
+  };
+
   # Set your time zone.
   time.timeZone = "Europe/London";
 
