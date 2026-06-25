a2b0345218894e09e933dbf8230d6eca8e5a96e9
Leo Collins <collins.leo@proton.me>
2026-06-25 19:50:18 +0100

    better ssh config

--- configuration.nix
+++ configuration.nix
@@ -133,7 +133,18 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+
+    settings = {
+      PermitRootLogin = "no";
+    };
+  };
+
+  users.users.leo.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2W1D1FLIjymY8HlxS4DyP9wrk/vLjouxuHaBHFDY/+ lac224@IC-X149F9H0WT"
+  ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
