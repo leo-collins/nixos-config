1829fee3386f3a79fe949f95987a124ace0963cb
Leo Collins <collins.leo@proton.me>
2026-06-30 23:59:36 +0100

    fonts; change style to monokai

--- home/leo/programs/alacritty.nix
+++ home/leo/programs/alacritty.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = [
+    pkgs.nerd-fonts.fira-code
+  ];
+
   programs.alacritty = {
     enable = true;
+    theme = "monokai";
+    settings = {
+      font = {
+        normal.family = "FiraCode Nerd Font";
+        bold.family = "FiraCode Nerd Font";
+        italic.family = "FiraCode Nerd Font";
+        bold_italic.family = "FiraCode Nerd Font";
+      };
+    };
   };
 }
