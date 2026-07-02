184ba37ab79f99ac40dd2fe8ca46cbf46170451c
Leo Collins <collins.leo@proton.me>
2026-07-02 10:29:50 +0100

    add bitwarden and ublock origin to zen browser
    Need to stop them from pinning to toolbar

--- home/leo/programs/zen-browser.nix
+++ home/leo/programs/zen-browser.nix
@@ -5,8 +5,19 @@
     inputs.zen-browser.homeModules.beta
   ];
 
-  programs.zen-browser = {
+  programs.zen-browser = let
+    mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+      installation_mode = "force_installed";
+      default_area = "navbar";
+    });
+  in {
     enable = true;
+
+    policies.ExtensionSettings = mkExtensionSettings {
+      "uBlock0@raymondhill.net" = "ublock-origin";
+      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
+    };
 
     profiles.default = {
       id = 0;
