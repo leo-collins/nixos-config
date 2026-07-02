98a3f01a7038fe820cce0cfc9e94de4d5afce925
Leo Collins <collins.leo@proton.me>
2026-07-02 23:32:46 +0100

    fix for cursor
    symlink into home directory
    fixes factorio and steam cursor issue
    This will need changing when switching themes or DEs

--- home/leo/default.nix
+++ home/leo/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -21,6 +21,19 @@
   home.username = "leo";
   home.homeDirectory = "/home/leo";
   home.stateVersion = "26.05";
+  
+  # symlink cursor icons into home directory.
+  # This fixes an issue where some apps cannot find the cursor
+  # and default to a different (non-scaled) cursor, e.g. in steam
+  # see: https://github.com/NixOS/nixpkgs/issues/437281
+  # https://github.com/ValveSoftware/steam-for-linux/issues/11484
+  # https://forums.factorio.com/113202
+  # NOTE: This does it for the KDE breeze theme; figure out a way to
+  # do this in general, e.g. for GNOME too
+  home.file.".local/share/icons/breeze_cursors" = {
+    source = "${pkgs.kdePackages.breeze}/share/icons/breeze_cursors/";
+    recursive = true;
+  };
 
   programs.home-manager.enable = true;
 }
