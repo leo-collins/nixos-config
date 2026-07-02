94db305124d7de16c3cf90c6091ea8dd54e519c4
Leo Collins <collins.leo@proton.me>
2026-07-02 19:16:50 +0100

    mount both SSDs on pc

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./modules/storage.nix
     ./modules/system.nix
     ./modules/networking.nix
     ./modules/desktop.nix
---
+++ modules/storage.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  fileSystems."/mnt/250gbssd" = {
+    device = "/dev/disk/by-uuid/9714a901-6663-4b15-84b4-c86d491ebb71";
+    fsType = "btrfs";
+    options = [ "nofail" "x-systemd.device-timeout=5s" ];
+  };
+  fileSystems."/mnt/1tbssd" = {
+    device = "/dev/disk/by-uuid/f5cbbccd-cac1-4062-9d85-48de8c917114";
+    fsType = "btrfs";
+    options = [ "nofail" "x-systemd.device-timeout=5s" ];
+  };
+}
