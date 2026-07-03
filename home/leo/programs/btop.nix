531d63c8ab4a09adcc2a14ee0f2c8587566d865f
Leo Collins <collins.leo@proton.me>
2026-07-03 11:16:43 +0100

    add gpu to btop

--- home/leo/programs/btop.nix
+++ home/leo/programs/btop.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.btop = {
     enable = true;
+ 
+    # Enable Nvidia GPU support
+    package = pkgs.btop.override {
+      cudaSupport = true;
+    };
   };
 }
