21bac43ade91e2a3a94f05ef956b2624ecf77a60
Leo Collins <collins.leo@proton.me>
2026-07-12 19:21:19 +0100

    create group leo

--- hosts/common/users/leo/default.nix
+++ hosts/common/users/leo/default.nix
@@ -2,10 +2,16 @@
 
 {
   users.users.leo = {
+    uid = 1000;
+    group = "leo";
     isNormalUser = true;
     description = "Leo";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
+  };
+
+  users.groups.leo = {
+    gid = 1000;
   };
 
   home-manager.users.leo =
