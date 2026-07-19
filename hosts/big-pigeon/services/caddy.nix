beee7631e8a4bca7e2753717c716ce223e6ceed2
Leo Collins <collins.leo@proton.me>
2026-07-19 13:02:43 +0100

    big-pigeon; setup ACME certs

--- hosts/big-pigeon/services/caddy.nix
+++ hosts/big-pigeon/services/caddy.nix
@@ -1,13 +1,30 @@
-{ ... }:
+{ config, ... }:
 
 {
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "collins.leo@proton.me";
+
+    certs."int.big-pigeon.com" = {
+      domain = "*.int.big-pigeon.com";
+      dnsProvider = "cloudflare";
+      credentialFiles = {
+        CLOUDFLARE_DNS_API_TOKEN_FILE = 
+          config.sops.secrets.cloudflare_dns_api_token.path;
+        };
+        group = "caddy";
+      };
+    };
+
   services.caddy = {
     enable = true;
     openFirewall = true;
 
-    virtualHosts."music.int.big-pigeon.com".extraConfig = ''
-      tls internal
-      reverse_proxy 127.0.0.1:4533
-    '';
+    virtualHosts."music.int.big-pigeon.com" = {
+      useACMEHost = "int.big-pigeon.com";
+      extraConfig = "reverse_proxy 127.0.0.1:4533";
+    };
   };
+
+  sops.secrets.cloudflare_dns_api_token = {};
 }
