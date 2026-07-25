111ee259ea7aa99060cc1fa552d62fb702c0b3d9
Leo Collins <collins.leo@proton.me>
2026-07-25 13:57:35 +0100

    beets initial config

--- home/leo/programs/cli/beets.nix
+++ home/leo/programs/cli/beets.nix
@@ -3,5 +3,11 @@
 {
   programs.beets = {
     enable = true;
+    settings = {
+      directory = "/tank/music";
+      library = "/home/leo/Documents/beets.db";
+      import.move = true;
+      plugins = [ "musicbrainz" ];
+    };
   };
 }
