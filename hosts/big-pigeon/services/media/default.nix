9ae4c8944b83448f98ca9f2cdc261de43e5b1bd5
Leo Collins <collins.leo@proton.me>
2026-07-26 14:47:37 +0100

    music download dir setgid

--- hosts/big-pigeon/services/media/default.nix
+++ hosts/big-pigeon/services/media/default.nix
@@ -15,4 +15,10 @@
   users.groups.media.gid = 986;
   users.users.leo.extraGroups = [ "media" ];
   users.users.plex.extraGroups = [ "media" ];
+
+  # New directories under the Soulseek download tree inherit `media`.
+  # File creation remains governed by slskd's 0002 service umask.
+  systemd.tmpfiles.rules = [
+    "d /tank/misc-storage/music 2775 - media -"
+  ];
 }
