f47cc7705889da3cd7f5fe7978e4ba34e26bcd29
Leo Collins <collins.leo@proton.me>
2026-07-26 20:44:19 +0100

    big-pigeon; caddy
    fixes

--- hosts/big-pigeon/services/caddy.nix
+++ hosts/big-pigeon/services/caddy.nix
@@ -34,6 +34,16 @@
       useACMEHost = "int.big-pigeon.com";
       extraConfig = "reverse_proxy 127.0.0.1:3000";
     };
+
+    virtualHosts."radarr.int.big-pigeon.com" = {
+      useACMEHost = "int.big-pigeon.com";
+      extraConfig = "reverse_proxy 127.0.0.1:7878";
+    };
+
+    virtualHosts."sonarr.int.big-pigeon.com" = {
+      useACMEHost = "int.big-pigeon.com";
+      extraConfig = "reverse_proxy 127.0.0.1:8989";
+    };
   };
 
   sops.secrets.cloudflare_dns_api_token = {};
