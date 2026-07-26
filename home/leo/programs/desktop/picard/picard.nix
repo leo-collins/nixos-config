6b1c5fdd40a7c4c65ddf392444ded22022b3588b
Leo Collins <collins.leo@proton.me>
2026-07-26 14:02:34 +0100

    replaygain picard

--- home/leo/programs/desktop/picard/picard.nix
+++ home/leo/programs/desktop/picard/picard.nix
@@ -1,8 +1,26 @@
 { config, pkgs, ... }:
 
+let
+  replaygain2Plugin = pkgs.fetchFromGitHub {
+    owner = "metabrainz";
+    repo = "picard-plugins";
+    rev = "efb04f6f9834a33573deb953ec3824f8bb828e15";
+    hash = "sha256-SzESA0LMGzpOJupTb2seGWBvmcZ/Dnwun4dTwXtgyXU=";
+  };
+
+  picardWithReplaygain2 = pkgs.picard.overrideAttrs (old: {
+    postInstall = (old.postInstall or "") + ''
+      pluginDir=$(echo "$out"/lib/python*/site-packages/picard/plugins)
+      mkdir -p "$pluginDir"
+      ln -s ${replaygain2Plugin}/plugins/replaygain2 \
+        "$pluginDir/replaygain2"
+    '';
+  });
+in
 {
   home.packages = [
-    pkgs.picard
+    picardWithReplaygain2
+    pkgs.rsgain
   ];
 
   home.file.".config/MusicBrainz/Picard.ini" = {
