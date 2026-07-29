520b67683f780aa125ef3c4e183d27409ec18c13
Leo Collins <collins.leo@proton.me>
2026-07-29 20:44:26 +0100

    add macbook home manager profile

---
+++ home/leo/macbook.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+# Home manager profile for imperial macbook
+
+{
+  imports = [
+    ./global
+  ];
+
+  home = {
+    username = "lac224";
+    homeDirectory = "/Users/lac224";
+  };
+}
