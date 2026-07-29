f9bd92d173fb2a69bd3397b89b8b4d0efdc32329
Leo Collins <collins.leo@proton.me>
2026-07-29 21:17:21 +0100

    make btop module more general

--- home/leo/programs/cli/btop.nix
+++ home/leo/programs/cli/btop.nix
@@ -1,12 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.btop = {
     enable = true;
  
-    # Enable Nvidia GPU support
-    package = pkgs.btop.override {
-      cudaSupport = true;
+    # Enable Nvidia GPU support, only on Linux
+    # TODO: this should be enabled only for Nvidia GPU hosts
+    package = pkgs.btop.override (
+      lib.optionalAttrs pkgs.stdenv.isLinux {
+        cudaSupport = true;
+      };
     };
   };
 }
