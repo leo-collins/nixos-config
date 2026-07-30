752d507972e55fcf537743b55b7586dfcd6aa9aa
Leo Collins <collins.leo@proton.me>
2026-07-30 22:26:21 +0100

    macbook; remove homebrew

--- home/leo/macbook.nix
+++ home/leo/macbook.nix
@@ -16,12 +16,4 @@
       "$HOME/.local/bin"
     ];
   };
-
-  programs.zsh = {
-    profileExtra = ''
-      if [[ -x /opt/homebrew/bin/brew ]]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      fi
-    '';
-  };
 }
