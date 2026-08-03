9016efd3254ed1bccb92ba230fd2b5a72073f552
Leo Collins <collins.leo@proton.me>
2026-08-03 23:15:17 +0100

    neovim tweaks
    show hidden in oil
    some neovim options

--- home/leo/programs/cli/neovim/oil.nix
+++ home/leo/programs/cli/neovim/oil.nix
@@ -4,6 +4,12 @@
   programs.nixvim.plugins = {
     oil = {
       enable = true;
+      settings = {
+        skip_confirm_for_simple_edits = true;
+        view_options = {
+          show_hidden = true;
+        };
+      };
     };
   };
 }
--- home/leo/programs/cli/neovim/options.nix
+++ home/leo/programs/cli/neovim/options.nix
@@ -5,22 +5,30 @@
     globals.mapleader = " ";
     vimAlias = true;
 
+    # https://neovim.io/doc/user/quickref/#option-list
     opts = {
       number = true;
       relativenumber = true;
       cursorline = true;
       signcolumn = "yes";
 
+      # spacing options
       expandtab = true;
       shiftwidth = 4;
       tabstop = 4;
 
-      ignorecase = true;
-      smartcase = true;
-      splitbelow = true;
+      # search options
+      ignorecase = true;  # ignore case in search pattern
+      smartcase = true;  # override `ignorecase` if the pattern contains upper case characters
+
+      # split options
+      splitbelow = true;  # splits (Ctrl-w s) are placed below
       splitright = true;
+
       termguicolors = true;
       undofile = true;
+      autoread = true;  # automatically updates buffers when edited outside of nvim
+      scrolloff = 10;  # minimum number of lines to keeo at top/bottom of screen
     };
   };
 }
