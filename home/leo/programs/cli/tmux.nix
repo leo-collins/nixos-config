4be50903ed54f229c3682e933d57edd49111714a
Leo Collins <collins.leo@proton.me>
2026-08-04 13:18:46 +0100

    enable tmuxp
    Store configs in ~/.config/tmuxp
    Home-manager didn't create this directory

--- home/leo/programs/cli/tmux.nix
+++ home/leo/programs/cli/tmux.nix
@@ -6,6 +6,7 @@
     mouse = true;
     clock24 = true;
     terminal = "tmux-256color";
+    tmuxp.enable = true;  # https://github.com/tmux-python/tmuxp
 #    extraConfig = ''
 #      set-option -g codepoint-widths[0] U+F313=1
 #    '';
