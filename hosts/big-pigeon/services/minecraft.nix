e26330d6d92c9b14d8ad159ae1196fa2a6b366e3
Leo Collins <collins.leo@proton.me>
2026-08-09 18:28:15 +0100

    minecraft server fixes

--- hosts/big-pigeon/services/minecraft.nix
+++ hosts/big-pigeon/services/minecraft.nix
@@ -1,5 +1,7 @@
 { inputs, pkgs, ... }:
 
+# https://github.com/Infinidoge/nix-minecraft/blob/master/README.md
+
 {
   imports = [
     inputs.nix-minecraft.nixosModules.minecraft-servers
@@ -9,14 +11,25 @@
     inputs.nix-minecraft.overlay
   ];
 
+  users.users.leo.extraGroups = [
+    "minecraft"
+  ];
+
   services.minecraft-servers = {
     enable = true;
     eula = true;
     openFirewall = true;
     servers.fabric = {
+      # To connect to server console, run:
+      # tmux -S /run/minecraft/fabric.sock attach
       enable = true;
+
+      autoStart = false;  # Start server with systemctl start minecraft-server-fabric
+      restart = "no";
       
-      package = pkgs.fabricServers.fabric-26_2;
+      package = pkgs.fabricServers.fabric-26_2.override {
+        jre_headless = pkgs.jdk25_headless;
+      };
     
       serverProperties = {
         # https://minecraft.wiki/w/Server.properties
