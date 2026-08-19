c13dbde7ff145cba9be05cba42a6d6e26b252a3a
Leo Collins <collins.leo@proton.me>
2026-08-19 10:55:53 +0100

    nh automatic garbage cleanup

--- home/leo/programs/cli/nh.nix
+++ home/leo/programs/cli/nh.nix
@@ -4,5 +4,9 @@
   programs.nh = {
     enable = true;
     flake = "${config.home.homeDirectory}/nixos";
+    clean = {
+      enable = true;
+      extraArgs = "--keep 5 --keep-since 3d --no-gc-roots";
+    };
   };
 }
