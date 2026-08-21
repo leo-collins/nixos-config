da80953175df4aa7ca08cc9c144205f3d8434ae7
Leo Collins <collins.leo@proton.me>
2026-08-21 17:16:06 +0100

    try to enable bit-perfect audio
    not sure if this did anything

--- hosts/common/optional/audio.nix
+++ hosts/common/optional/audio.nix
@@ -9,5 +9,19 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire."10-bit-perfect" = {
+      "context.properties" = {
+        "default.clock.rate" = 44100;
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          88200
+          96000
+          176400
+          192000
+        ];
+      };
+    };
   };
 }
