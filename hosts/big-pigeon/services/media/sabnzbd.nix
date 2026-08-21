c44dd4e8c2b8c162be56c76832742f684a872308
Leo Collins <collins.leo@proton.me>
2026-08-21 22:55:03 +0100

    sab; add caddy reverse proxy

--- hosts/big-pigeon/services/media/sabnzbd.nix
+++ hosts/big-pigeon/services/media/sabnzbd.nix
@@ -42,6 +42,8 @@
         direct_unpack = 1;
         unwanted_extensions = "ade, adp, app, application, appref-ms, asp, aspx, asx, bas, bat, bgi, cab, cer, chm, cmd, cnt, com, cpl, crt, csh, der, diagcab, exe, fxp, gadget, grp, hlp, hpj, hta, htc, inf, ins, iso, isp, its, jar, jnlp, js, jse, ksh, lnk, mad, maf, mag, mam, maq, mar, mas, mat, mau, mav, maw, mcf, mda, mdb, mde, mdt, mdw, mdz, msc, msh, msh1, msh2, mshxml, msh1xml, msh2xml, msi, msp, mst, msu, ops, osd, pcd, pif, pl, plg, prf, prg, printerexport, ps1, ps1xml, ps2, ps2xml, psc1, psc2, psd1, psdm1, pst, py, pyc, pyo, pyw, pyz, pyzw, reg, scf, scr, sct, shb, shs, sln, theme, tmp, url, vb, vbe, vbp, vbs, vcxproj, vhd, vhdx, vsmacros, vsw, webpnp, website, ws, wsc, wsf, wsh, xbap, xll, xnk";
         action_on_unwanted_extensions = 2;  # Fail job (move to history)
+        inet_exposure = 5;
+        host_whitelist = "sabnzbd.int.big-pigeon.com";
       };
 
       servers.newshosting = {
@@ -57,4 +59,9 @@
 
   # Make newly created files be writable by `media`
   systemd.services.sabnzbd.serviceConfig.UMask = "0002";
+
+  services.caddy.virtualHosts."sabnzbd.int.big-pigeon.com" = {
+    useACMEHost = "int.big-pigeon.com";
+    extraConfig = "reverse_proxy 127.0.0.1:8080";
+  };
 }
