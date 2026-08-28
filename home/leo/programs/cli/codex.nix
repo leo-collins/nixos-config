7ca7de845cb6201e8699e1b6b3fb31c09db4602f
Leo Collins <collins.leo@proton.me>
2026-08-28 09:02:52 +0100

    fix minted

--- home/leo/programs/cli/codex.nix
+++ home/leo/programs/cli/codex.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = [
@@ -13,17 +13,4 @@
       command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
     };
   };
-
-  # Preserve a config created by the old programs.codex module when switching
-  # to a package-only setup. New configs are created and managed by Codex.
-  home.activation.migrateCodexConfig =
-    lib.hm.dag.entryBetween [ "linkGeneration" ] [ "writeBoundary" ] ''
-      codexConfig="$HOME/.codex/config.toml"
-
-      if [ -L "$codexConfig" ]; then
-        codexConfigSource="$(readlink -e "$codexConfig")"
-        run cp -- "$codexConfigSource" "$codexConfig.hm-mutable"
-        run mv -- "$codexConfig.hm-mutable" "$codexConfig"
-      fi
-    '';
 }
--- home/leo/programs/cli/neovim/vimtex.nix
+++ home/leo/programs/cli/neovim/vimtex.nix
@@ -4,6 +4,15 @@
 # https://github.com/lervag/vimtex
 # https://nix-community.github.io/nixvim/plugins/vimtex/index.html
 
+# workaround for https://github.com/NixOS/nixpkgs/issues/542483
+let
+  texlivePkgs = pkgs.texlive.override {
+    python3 = pkgs.python313;
+    python3Packages = pkgs.python313Packages;
+    texlive = texlivePkgs;
+  };
+  texliveWithMintedFix = texlivePkgs.schemes.texliveFull;
+in
 {
   programs.nixvim.plugins = {
     vimtex = {
@@ -15,8 +24,8 @@
         compiler_method = "latexmk";
       };
 
-      # use texliveFull. Warning: this brings in a lot of packages
-      texlivePackage = pkgs.texliveFull;
+      # use pkgs.texliveFull. Warning: this brings in a lot of packages
+      texlivePackage = texliveWithMintedFix;
     };
 
     # add omni to tex blink completions. The other ones are the defaults
