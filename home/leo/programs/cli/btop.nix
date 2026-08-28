ae1b89a40cf113d9944982a78ce0b6c737d8a912
Leo Collins <collins.leo@proton.me>
2026-08-28 09:05:16 +0100

    fix warning

--- home/leo/programs/cli/btop.nix
+++ home/leo/programs/cli/btop.nix
@@ -7,7 +7,7 @@
     # Enable Nvidia GPU support, only on Linux
     # TODO: this should be enabled only for Nvidia GPU hosts
     package = pkgs.btop.override (
-      lib.optionalAttrs pkgs.stdenv.isLinux {
+      lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
         cudaSupport = true;
       }
     );
