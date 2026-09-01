3c06534fc71dade47937309b5a7b428c19fd1d2a
Leo Collins <collins.leo@proton.me>
2026-09-01 15:21:51 +0100

    mallard; add a file manager

--- home/leo/mallard.nix
+++ home/leo/mallard.nix
@@ -8,6 +8,7 @@
     # ./programs/desktop/feishin.nix
     ./programs/desktop/niri.nix
     ./programs/desktop/noctalia.nix
+    ./programs/desktop/nautilus.nix
 
     ./programs/desktop/slack.nix
     ./programs/desktop/obsidian.nix
---
+++ home/leo/programs/desktop/nautilus.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.nautilus
+  ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+    };
+  };
+}
