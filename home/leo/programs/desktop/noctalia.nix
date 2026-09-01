9a39c942353b7d1546fff4d72bb5a67909321312
Leo Collins <collins.leo@proton.me>
2026-09-01 15:47:20 +0100

    update noctalia settings

--- home/leo/programs/desktop/noctalia.nix
+++ home/leo/programs/desktop/noctalia.nix
@@ -7,6 +7,37 @@
 
     settings = {
       shell.font_family = "sans-serif";
+
+      theme = {
+        source = "builtin";
+        builtin = "Gruvbox";
+      };
+
+      wallpaper = {
+        directory = "/home/leo/Pictures/Wallpapers";
+        automation = {
+          enabled = true;
+          interval_seconds = 14400;
+        };
+      };
+
+      dock = {
+        enabled = true;
+        auto_hide = false;
+        smart_auto_hide = true;
+        reserve_space = false;
+        show_dots = true;
+        show_instance_count = false;
+      };
+
+      location.auto_locate = true;
+
+      bar.default = {
+        thickness = 28;
+        scale = 1.05;
+      };
+
+      widget.clock.format = "{:%H:%M} - {:%F (%a)}";
     };
   };
 }
