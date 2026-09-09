4eb0ce45fc2ffb8d6d12e1625062b8123749ac26
Leo Collins <collins.leo@proton.me>
2026-09-09 13:53:21 +0100

    little-pigeon; enable intel graphics drivers

--- hosts/little-pigeon/system.nix
+++ hosts/little-pigeon/system.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   system.stateVersion = "26.05";
+
+  services.thermald.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = [
+      pkgs.intel-media-driver
+      pkgs.intel-ocl
+      pkgs.intel-vaapi-driver
+    ];
+  };
 }
