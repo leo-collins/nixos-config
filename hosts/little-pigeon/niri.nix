686c204c083182cbdeaee03724058a451b9d178d
Leo Collins <collins.leo@proton.me>
2026-09-16 14:03:56 +0100

    little-pigeon; set keyboard

--- hosts/little-pigeon/niri.nix
+++ hosts/little-pigeon/niri.nix
@@ -1,6 +1,23 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
+  # apple aluminium keyboard a1243
+  services.xserver.xkb.model = "applealu_iso";
+  services.xserver.xkb.variant = lib.mkForce "mac";
+
+  home-manager.users.leo.home.packages = [ pkgs.wtype ];
+
+  home-manager.users.leo.programs.niri.settings.input.keyboard.xkb = {
+    model = "applealu_iso";
+    layout = "gb";
+    variant = "mac";
+  };
+
+  home-manager.users.leo.programs.niri.settings.binds."Alt+3" = {
+    action.spawn = [ "wtype" "#" ];
+    hotkey-overlay.hidden = true;
+  };
+
   home-manager.users.leo.programs.niri.settings.outputs = {
     # ultrawide monitor
     "Dell Inc. DELL S3422DWG GL6SS63" = {
